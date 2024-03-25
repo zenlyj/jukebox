352fb3c5f410fc4923f80aeaa69d5e6cf9263ebf
Zen <iarezenly@gmail.com>
2024-03-25 21:46:36 +0800

    Improve promise chain

--- frontend/src/api/api.tsx
+++ frontend/src/api/api.tsx
@@ -1,63 +1,66 @@
 const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 const accessToken = (): string | null => sessionStorage.getItem("access_token");
-const refreshToken = (): string | null => sessionStorage.getItem("refresh_token");
+const refreshToken = (): string | null =>
+  sessionStorage.getItem("refresh_token");
 
-async function authorize(): Promise<void> {
+function authorize(): void {
   const windowURL = window.location.search;
   const params = new URLSearchParams(windowURL);
   const authCode = params.get("code");
-  const token = accessToken()
+  const token = accessToken();
 
   if (token !== null) {
-    accessTokenExpiryCheck(token);
+    isAccessTokenExpired(token).then((isExpired) => {
+      if (isExpired) {
+        refreshAccessToken();
+      }
+    });
   }
   if (token === null && authCode !== null) {
-    return fetch(
-      `${SERVER_URL}/spotify/authorize?authorization_code=${authCode}`
-    ).then((value) =>
-      Promise.resolve(value.json()).then((value) => {
+    fetch(`${SERVER_URL}/spotify/authorize?authorization_code=${authCode}`)
+      .then((value) => value.json())
+      .then((value) => {
         sessionStorage.setItem("access_token", value.access_token);
         sessionStorage.setItem("refresh_token", value.refresh_token);
-      })
-    );
+      });
   }
 }
 
-const accessTokenExpiryCheck = async (accessToken: string): Promise<void> => {
-  fetch(
+const isAccessTokenExpired = async (accessToken: string): Promise<boolean> => {
+  return fetch(
     `${SERVER_URL}/spotify/search?` +
       new URLSearchParams({
         query: "test",
         query_type: "track",
         access_token: accessToken,
       })
-  ).then((response) => {
-    if (response.status === 200) return;
-    response.json().then((response) => {
-      if (response.detail === "The access token expired") {
-        refreshAccessToken();
-      }
-    });
-  });
+  )
+    .then((response) => (response.status !== 200 ? response.json() : null))
+    .then((response) => !!(response?.detail === "The access token expired"));
 };
 
-const refreshAccessToken = async (): Promise<void> => {
+const refreshAccessToken = (): void => {
   const url =
     `${SERVER_URL}/spotify/authorize/refresh/?` +
     new URLSearchParams({
       refresh_token: refreshToken() ?? "",
     });
 
-  fetch(url).then((response) => {
-    if (response.status !== 200) {
-      console.log("Unable to refresh access token");
-      return;
-    }
-    response.json().then((response) => {
-      sessionStorage.setItem("access_token", response.access_token);
-      console.log("Access Token refreshed");
+  fetch(url)
+    .then((response) => {
+      if (response.status !== 200) {
+        console.log("Unable to refresh access token");
+        return null;
+      } else {
+        return response.json();
+      }
+    })
+    .then((response) => {
+      if (response) {
+        sessionStorage.setItem("access_token", response.access_token);
+        console.log("Access Token refreshed");
+      }
     });
-  });
 };
 
 async function getSongs(): Promise<Response> {
@@ -83,7 +86,9 @@ async function getPlaylist(): Promise<Response | undefined> {
   }
 }
 
-async function deletePlaylistSong(songId: string): Promise<Response | undefined> {
+async function deletePlaylistSong(
+  songId: string
+): Promise<Response | undefined> {
   if (accessToken() !== null) {
     return fetch(
       `${SERVER_URL}/playlist/?session=${accessToken()}&song_id=${songId}`,
