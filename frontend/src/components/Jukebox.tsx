fbe8438733840040b2f525828b4b3c0304b762f2
Zen <iarezenly@gmail.com>
2024-04-22 00:13:00 +0800

    Use reducer hook in Jukebox and Playlist

--- frontend/src/components/Jukebox.tsx
+++ frontend/src/components/Jukebox.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer } from "react";
 import MusicList from "./MusicList.tsx";
 import { MusicListPagination } from "./MusicListPagination.tsx";
 import { Song } from "./models/Song.tsx";
@@ -11,22 +11,70 @@ import { useOutletContext } from "react-router-dom";
 import { HomeContext } from "./models/HomeContext.tsx";
 import { Box } from "@mui/material";
 
+interface State {
+  songs: Song[];
+  songCount: number;
+  pageNum: number;
+}
+
+enum ActionType {
+  GET_RECOMMENDED_SONGS,
+  CHANGE_PAGE_NUMBER,
+}
+
+interface GetRecommendedSongsAction {
+  type: ActionType.GET_RECOMMENDED_SONGS;
+  songs: Song[];
+  songCount: number;
+}
+
+interface ChangePageNumberAction {
+  type: ActionType.CHANGE_PAGE_NUMBER;
+  pageNum: number;
+}
+
+type Action = GetRecommendedSongsAction | ChangePageNumberAction;
+
+function reducer(state: State, action: Action): State {
+  switch (action.type) {
+    case ActionType.GET_RECOMMENDED_SONGS:
+      return {
+        ...state,
+        songs: action.songs,
+        songCount: action.songCount,
+      };
+    case ActionType.CHANGE_PAGE_NUMBER:
+      return {
+        ...state,
+        pageNum: action.pageNum,
+      };
+    default:
+      return state;
+  }
+}
+
 function Jukebox() {
-  const [songs, setSongs] = useState<Song[]>([]);
-  const [songCount, setSongCount] = useState<number>(0);
-  const [pageNum, setPageNum] = useState<number>(1);
+  const [state, dispatch] = useReducer(reducer, {
+    songs: [],
+    songCount: 0,
+    pageNum: 1,
+  });
+
   const { playlistSize, setPlaylistSize, genre } =
     useOutletContext<HomeContext>();
   const pageSize = 10;
 
   useEffect(() => {
-    getRecommendedSongs(pageNum);
-  }, [pageNum]);
+    getRecommendedSongs(state.pageNum);
+  }, [state.pageNum]);
 
   const getRecommendedSongs = (pageNum: number) => {
     getSongs(genre, pageNum, pageSize).then((response: GetSongsResponse) => {
-      setSongs(response.songs);
-      setSongCount(response.songCount);
+      dispatch({
+        type: ActionType.GET_RECOMMENDED_SONGS,
+        songs: response.songs,
+        songCount: response.songCount,
+      });
     });
   };
 
@@ -42,11 +90,11 @@ function Jukebox() {
   };
 
   const handlePageChange = (event, pageNumber: number): void => {
-    setPageNum(pageNumber);
+    dispatch({ type: ActionType.CHANGE_PAGE_NUMBER, pageNum: pageNumber });
   };
 
   const getPageCount = (): number => {
-    return Math.ceil(songCount / pageSize);
+    return Math.ceil(state.songCount / pageSize);
   };
 
   return (
@@ -60,13 +108,13 @@ function Jukebox() {
       }}
     >
       <MusicList
-        songs={songs}
+        songs={state.songs}
         displayDate={true}
         onClickHandler={addToPlaylist}
       />
       <MusicListPagination
         pageCount={getPageCount()}
-        pageNum={pageNum}
+        pageNum={state.pageNum}
         handlePageChange={handlePageChange}
       />
     </Box>
